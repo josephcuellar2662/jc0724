cb30fb570da95fa2a91ac074f635f648464018e1
josephcuellar <josephcuellar2662@gmail.com>
2023-07-25 10:13:25 -0500

    added business logic to checkout service

--- homedepot/src/main/java/service/CheckoutService.java
+++ homedepot/src/main/java/service/CheckoutService.java
@@ -2,9 +2,11 @@ package service;
 
 import model.Tool;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.time.temporal.TemporalAdjusters;
 
 public class CheckoutService {
 
@@ -21,36 +23,84 @@ public class CheckoutService {
         System.out.println("Due date: " + formattedDate);
 
 
-        System.out.println("Daily rental charge: " + tool.getDailyCharge());
-        System.out.println("Charge days: " + countChardDays());
-        System.out.println("Pre-discount charge: " + calculatePreDiscountCharge());
+        System.out.println("Daily rental charge: $" + tool.getDailyCharge());
+
+        int chargeDays = countChargeDays(tool, checkoutDate, numberOfDays);
+        System.out.println("Charge days: " + chargeDays);
+
+        double preDiscountCharge = calculatePreDiscountCharge(tool, chargeDays);
+        System.out.println("Pre-discount charge: $" + String.format("%.2f", preDiscountCharge));
         System.out.println("Discount percent: " + discount + "%");
-        System.out.println("Discount amount: " + calculateDiscountAmount());
-        System.out.println("Final Charge: " + calculateFinalCharge());
+
+        double discountAmount = calculateDiscountAmount(discount, preDiscountCharge);
+        System.out.println("Discount amount: " + String.format("%.2f", discountAmount));
+
+        System.out.println("Final Charge: $" + String.format("%.2f", (preDiscountCharge - discountAmount)));
     }
 
-    private int countChardDays() {
+    private int countChargeDays(Tool tool, LocalDate checkoutDate, int numberOfDays) {
+        int count = 0;
+        DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
+        for(int i=1; i<=numberOfDays; i++) {
 
-        return 0;
+            String dayOfWeek = checkoutDate.plusDays(i).getDayOfWeek().toString();
+            boolean isWeekend = checkIsWeekend(dayOfWeek);
+            boolean isHoliday = checkIsHoliday(checkoutDate);
+
+            if (tool.isWeekendCharge() && isWeekend) {
+                count += 1;
+            }
+            else if (tool.isWeekdayCharge() && !isWeekend) {
+                count += 1;
+            }
+            else if (tool.isHolidayCharge() && isHoliday) {
+                count += 1;
+            }
+
+        }
+
+        return count;
     }
 
-    private double calculatePreDiscountCharge() {
+    private boolean checkIsWeekend(String dayOfWeek) {
 
-        return 0.0;
+        if (dayOfWeek.equalsIgnoreCase("SATURDAY") || dayOfWeek.equalsIgnoreCase("SUNDAY")) {
+            return true;
+        }
+
+        return false;
     }
 
-    private double calculateDiscountAmount() {
-        return 0.0;
+    private boolean checkIsHoliday(LocalDate date) {
+        String month = date.getMonth().toString();
+        String dayOfWeek = date.getDayOfWeek().toString();
+        int dayOfMonth = date.getDayOfMonth();
+
+        // Independence Day
+        if (month.equalsIgnoreCase("July") && dayOfMonth == 4) {
+            return true;
+        }
+
+        // Labor Day
+        LocalDate firstMonday = date.with(TemporalAdjusters.firstInMonth(DayOfWeek.MONDAY));
+        if (month.equalsIgnoreCase("September") && dayOfWeek.equalsIgnoreCase(firstMonday.getDayOfWeek().toString())) {
+            return true;
+        }
+
+        return false;
     }
 
-    private double calculateFinalCharge() {
-        return 0.0;
+    private double calculatePreDiscountCharge(Tool tool, int chargeDays) {
+
+        return tool.getDailyCharge() * chargeDays;
     }
 
-//    public static void main(String args[]) {
-//        LocalDate checkoutDate = LocalDate.now();
-//        DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
-//        String formattedDate = formatter.format(checkoutDate.plusDays(3));
-//        System.out.println("Due date: " + formattedDate);
-//    }
+    private double calculateDiscountAmount(int discount, double preDiscountCharge) {
+        return preDiscountCharge * ((double)discount/100);
+    }
+
+    public static void main(String args[]) {
+        CheckoutService checkoutService = new CheckoutService();
+        checkoutService.checkout(Tool.JAKR, LocalDate.now(), 5, 100);
+    }
 }
